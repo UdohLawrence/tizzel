18cc20b6d467c6194791719e60f604954be641ce
EkomobongLawrence <lawrenceaudoh@gmail.com>
2025-01-26 21:15:09 +0100

    setup shadcn

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+const RootLayout = ({children,}: Readonly<{children: React.ReactNode;}>) => {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1 wrapper">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default RootLayout;
