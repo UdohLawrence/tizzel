56e6be528b889cd7a40fa975e10f45818c5d3991
EkomobongLawrence <lawrenceaudoh@gmail.com>
2025-01-26 23:11:47 +0100

    implement the loading page

---
+++ app/loading.tsx
@@ -0,0 +1,25 @@
+import loader from '@/assets/loader.gif';
+import Image from 'next/image';
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image
+        src={loader}
+        width={150}
+        height={150}
+        alt='loading'
+      />
+    </div>    
+  )
+}
+
+export default Loading
