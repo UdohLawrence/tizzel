24516c36b6e14ff2a9dde9c5c525185ed4e6aaf7
EkomobongLawrence <lawrenceaudoh@gmail.com>
2025-02-01 04:52:30 +0100

    UPDATE: get latest products from the database, instead of the sample data

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,7 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Tizzel Stores'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Tizzel Stores';
 
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Discover everything you need for your home at our online store! From kitchen essentials and cleaning supplies to stylish decor and cozy bedding, find top-quality household goods at unbeatable prices.";
 
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 6;
